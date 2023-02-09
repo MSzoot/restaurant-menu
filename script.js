0d03bf442707f08d862e682032eea89b6cec3667
MSzoot <97002088+MSzoot@users.noreply.github.com>
2023-02-09 17:56:58 +0000

    order list update not finished

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 import { menuArray } from "./data.js";
 
+document.addEventListener('click', function(e){
+    if (e.target.id){
+        addItem(e.target.id)
+    }
+    else if(e.target.id == `remove-${id}`){
+        console.log(e.target.id)
+    }
+})
+
+let basket = []
+
+let addItem = (id) => {
+    basket.push(menuArray[id])
+    renderOrder();
+
+}
 
 
 
@@ -13,12 +29,7 @@ import { menuArray } from "./data.js";
 
 
 
-
-
-
-
-
-let render = () => {
+let rendermenu = () => {
 
     for (let product of menuArray){
         document.getElementById("menu").innerHTML += 
@@ -40,7 +51,17 @@ let render = () => {
       </button>
     </div>
   </div>`
-    }
+    } 
 }
 
-render()
+rendermenu()
+
+
+let renderOrder = () =>{
+        document.getElementById("basket").innerHTML += `
+        <div class="w-10/12 mx-auto flex items-center gap-10 pb-2">
+        <h1 class="text-2xl font-bold">${basket[basket.length-1].name}</h1>
+        <button id="remove-${basket[basket.length-1].id.value}"class="text-sm text-red-600">X</button>
+        <p class="font-bold ml-auto text-lg">£${basket[basket.length-1].price}</p>
+      </div>`
+    }
