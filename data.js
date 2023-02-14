c970d5742b37f21edc4c928922d2e85890f55f96
MSzoot <97002088+MSzoot@users.noreply.github.com>
2023-02-14 17:21:00 +0000

    working basket upload

--- data.js
+++ data.js
@@ -3,20 +3,20 @@ export const menuArray = [
         name: "Pizza",
         ingredients: ["pepperoni", "mushrom", "mozarella"],
         id: 0,
-        price: 14,
+        price: 12,
         emoji: "🍕"
     },
     {
         name: "Hamburger",
         ingredients: ["beef", "cheese", "lettuce"],
-        price: 12,
+        price: 7,
         emoji: "🍔",
         id: 1
     },
         {
         name: "Beer",
         ingredients: ["grain, hops, yeast, water"],
-        price: 12,
+        price: 6,
         emoji: "🍺",
         id: 2
     }
--- script.js
+++ script.js
@@ -1,39 +1,35 @@
 import { menuArray } from "./data.js";
+let basket = [];
 
 document.addEventListener('click', function(e){
-    if (e.target.id){
+    if (e.target.id && e.target.id == 0 || e.target.id == 1 || e.target.id == 2){
         addItem(e.target.id)
     }
-    else if(e.target.id == `remove-${id}`){
-        console.log(e.target.id)
+    else if(e.target.dataset.delete){
+        removeItem(e.target.dataset.delete)
     }
 })
 
-let basket = []
 
 let addItem = (id) => {
     basket.push(menuArray[id])
-    renderOrder();
+    render();
 
 }
 
 
+let removeItem = (RemoveId) => {
+    const indexToRemove = basket.findIndex(obj => obj.id == RemoveId);
+    basket.splice(indexToRemove,1);   
+    render();
+}
 
 
-
-
-
-
-
-
-
-
-
-let rendermenu = () => {
-
-    for (let product of menuArray){
-        document.getElementById("menu").innerHTML += 
-    `<div
+let gethtmlFeed = () => {
+  let htmlFeed = ''
+  
+  for (let product of menuArray){
+    htmlFeed += `<div
     class="w-10/12 mx-auto flex items-center py-10 border-b border-slate-400-b"
   >
     <span class="text-6xl w-2/12">${product.emoji}</span>
@@ -44,24 +40,56 @@ let rendermenu = () => {
         <p class="font-bold">£${product.price}</p>
       </div>
       <button
-        id = ${product.id}
+        id ="${product.id}"
         class="border border-slate-400 rounded-full px-5 py-3 text-2xl"
       >
         +
       </button>
     </div>
   </div>`
-    } 
+  
+  }
+  return htmlFeed
 }
 
-rendermenu()
+let getBasketFeed = () => {
+  let basketFeed = ''
+  if(basket.length > 0){
+  basketFeed = '<h1 class="text-center mt-10 text-3xl">Your order</h1>'
+
+  for (let basketItem of basket) {
+    basketFeed += `<div class="w-10/12 mx-auto flex items-center gap-10 pb-2">
+    <h1 class="text-2xl font-bold">${basketItem.name}</h1>
+    <button class="text-sm  text-red-600" data-delete="${basketItem.id}">X</button>
+    <p class="font-bold ml-auto text-lg">£${basketItem.price}</p>
+  </div>`
+  }
+}
+return basketFeed
+}
 
 
-let renderOrder = () =>{
-        document.getElementById("basket").innerHTML += `
-        <div class="w-10/12 mx-auto flex items-center gap-10 pb-2">
-        <h1 class="text-2xl font-bold">${basket[basket.length-1].name}</h1>
-        <button id="remove-${basket[basket.length-1].id.value}"class="text-sm text-red-600">X</button>
-        <p class="font-bold ml-auto text-lg">£${basket[basket.length-1].price}</p>
-      </div>`
-    }
+let getTotalFeed = () =>{
+  let totalFeed = '';
+  let totalPrice = 0
+  if(basket.length > 0){
+  for (let basketItem of basket){
+    totalPrice += basketItem.price ;
+    totalFeed = `<div class="w-10/12 mx-auto flex items-center py-4 mt-10 border-t-2">
+    <h1 class="text-2xl font-bold">Total :</h1>
+    <p class="font-bold ml-auto text-2xl">£${totalPrice}</p>
+  </div>
+  `
+  }
+}
+return totalFeed;
+}
+
+let render = () => {
+    document.getElementById("menu").innerHTML = gethtmlFeed()
+    document.getElementById("menu").innerHTML += getBasketFeed()
+    document.getElementById("menu").innerHTML += getTotalFeed()
+    
+}
+
+render() 
